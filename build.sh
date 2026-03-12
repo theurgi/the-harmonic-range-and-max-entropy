d9a1076aaba032f3f90c3d3ec39308418ad2c576
theurgi <58859663+theurgi@users.noreply.github.com>
2026-03-12 17:47:56 -0400

    fix build script and update license date

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-typst compile src/the-harmonic-range-and-maximum-entropy.typ build/
+typst compile src/the-harmonic-range-and-maximum-entropy.typ build/the-harmonic-range-and-maximum-entropy.pdf
